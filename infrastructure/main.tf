288c3fef4c4c8198f7600937fc368a5d20c1b6f0
Zmohamed6991 <zmohamed6991@gmail.com>
2025-02-18 17:50:58 +0500

    update postgres version in terraform as 15.3 is no longer supported on RDS

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -139,7 +139,7 @@ resource "aws_db_instance" "postgres" {
   allocated_storage      = 20
   storage_type          = "gp2"
   engine                = "postgres"
-  engine_version        = "15.3"
+  engine_version        = "15.4"
   instance_class        = "db.t3.micro"
   db_name               = "hstc"
   username              = "postgres"
